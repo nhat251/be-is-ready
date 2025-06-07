9bfb3981eb97c8530c585c4f079a2e5d57665f6d
NhatNS <nsn251tp@gmail.com>
2025-06-07 16:16:56 +0700

    á

--- src/main/java/org/codewith3h/finmateapplication/config/CorsConfig.java
+++ src/main/java/org/codewith3h/finmateapplication/config/CorsConfig.java
@@ -1,15 +1,15 @@
-//package org.codewith3h.finmateapplication.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/api/**")
-//            .allowedOrigins("https://your-vercel-app.vercel.app") // đổi thành URL FE thật
-//            .allowedMethods("*");
-//    }
-//}
+package org.codewith3h.finmateapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/FinMateApplication/**")
+            .allowedOrigins("https://test-deploy-fe-vert.vercel.app/") // đổi thành URL FE thật
+            .allowedMethods("*");
+    }
+}
